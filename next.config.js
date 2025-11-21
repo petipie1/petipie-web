d936c58c45acbf1c6fe1e9bfdefbbd76d4deff23
Dorian Musaj <dorianmusaj@gmail.com>
2025-11-22 00:57:05 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const nextConfig = {
     domains: ["freepngimg.com", "res.cloudinary.com"],
   },
   output: "export",
-  basePath: "petipie-web",
+    basePath: "/petipie-web",
+  assetPrefix: "/petipie-web/",
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
     API_KEY: process.env.API_KEY,
